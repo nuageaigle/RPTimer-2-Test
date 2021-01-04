bb9a4a0642723d36aad09e8cb0f188766ce4c3c8
nuage <nuageaigle@gmail.com>
2021-01-04 18:58:52 +0800

    Initial Commit

---
+++ RPTimer/RPTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPTimerApp.swift
+//  RPTimer
+//
+//  Created by aigle nuage on 2021/1/4.
+//
+
+import SwiftUI
+
+@main
+struct RPTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
